a2ad57356b0a47ddcdba7d468e66c36878717deb
A Bee Small <16360973+pLTdwaxn@users.noreply.github.com>
2026-07-11 13:16:30 +0930

    NextJS allowed dev origins to load from env

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.0.6"],
+  allowedDevOrigins:
+    process.env.ALLOWED_DEV_ORIGINS?.split(",").map((origin) =>
+      origin.trim(),
+    ) ?? [],
 };
 
 export default nextConfig;
