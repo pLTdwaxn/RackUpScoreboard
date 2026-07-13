5cfc7a54da0aaae5cc15a8857a19a5ebbabce48a
A Bee Small <16360973+pLTdwaxn@users.noreply.github.com>
2026-07-13 14:33:51 +0930

    Update deployment script

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <Surface
       variant="tertiary"
-      className="mx-auto flex h-dvh w-full max-w-md flex-col overflow-hidden p-0"
+      className="flex mx-auto h-dvh w-full max-w-md flex-col overflow-hidden p-0 items-center justify-center"
     >
       <Button onClick={() => (window.location.href = "/matchroom")}>
         Snooker Scoreboard
