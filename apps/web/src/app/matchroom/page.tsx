369b5ff91fafcf618f931787c3586525fa32feeb
A Bee Small <16360973+pLTdwaxn@users.noreply.github.com>
2026-07-19 12:01:24 +0930

    Fix app height issue

--- apps/web/src/app/matchroom/page.tsx
+++ apps/web/src/app/matchroom/page.tsx
@@ -3,5 +3,9 @@
 import { LobbyCard } from "@/components/Lobby";
 
 export default function AppPage() {
-  return <LobbyCard />;
+  return (
+    <div className="flex h-full w-full items-center justify-center p-4">
+      <LobbyCard />
+    </div>
+  );
 }
